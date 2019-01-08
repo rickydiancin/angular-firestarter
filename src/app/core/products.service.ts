e61ef6355c1a65fd60d02bf34ca2d8c6066ed566
Ricky Noel <rickynoel@Rickys-MacBook-Pro.local>
2019-01-08 22:45:54 +0800

    product service

--- src/app/core/products.service.ts
+++ src/app/core/products.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
 
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  productsCollection: AngularFirestoreCollection<any>;
+  productDocument:   AngularFirestoreDocument<any>;
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) {
+    this.productsCollection = this.afs.collection('products', (ref) => ref.orderBy('time', 'desc').limit(5));
+  }
+
+  getData(): Observable<any[]> {
+    // ['added', 'modified', 'removed']
+    return this.productsCollection.snapshotChanges().pipe(
+      map((actions) => {
+        return actions.map((a) => {
+          const data = a.payload.doc.data();
+          return { id: a.payload.doc.id, ...data };
+        });
+      })
+    );
+  }
+
+  getProduct(id: string) {
+    return this.afs.doc<any>(`products/${id}`);
+  }
+
+  createProduct(content: string) {
+    const product = {
+      content,
+      hearts: 0,
+      time: new Date().getTime(),
+    };
+    return this.productsCollection.add(product);
+  }
+
+  updateProduct(id: string, data: any) {
+    return this.getProduct(id).update(data);
+  }
+
+  deleteProduct(id: string) {
+    return this.getProduct(id).delete();
+  }
 }
