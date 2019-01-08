455196474c0c18327675168a478e9a1d5a2a9fc5
Ricky Noel <rickynoel@Rickys-MacBook-Pro.local>
2019-01-08 21:11:46 +0800

    new db

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyAc9Tt8pQQF3ntu2Tz66iUKl_fXHtdhIyc',
-    authDomain: 'firestarter-96e46.firebaseapp.com',
-    databaseURL: 'https://firestarter-96e46.firebaseio.com',
-    projectId: 'firestarter-96e46',
-    storageBucket: 'firestarter-96e46.appspot.com',
-    messagingSenderId: '581326886241'
+    apiKey: "AIzaSyDbkwnutDsJ-Glqi3KpwYvC4rTcqjpXKX8",
+    authDomain: "gentec-admin.firebaseapp.com",
+    databaseURL: "https://gentec-admin.firebaseio.com",
+    projectId: "gentec-admin",
+    storageBucket: "gentec-admin.appspot.com",
+    messagingSenderId: "188938269212"
   }
+
 };
 
 /*
