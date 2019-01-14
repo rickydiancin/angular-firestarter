014fd05133135a9a5be1fffb42d79aa8e273b1a2
Ricky Noel <rickynoel@Rickys-MacBook-Pro.local>
2019-01-14 13:28:13 +0800

    homepage category

--- src/app/ui/home-page/home-page.component.ts
+++ src/app/ui/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ScriptsService } from 'src/app/core/scripts.service';
+import { ProductsService } from 'src/app/core/products.service';
 
 
 @Component({
@@ -8,15 +9,21 @@ import { ScriptsService } from 'src/app/core/scripts.service';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+  categories:any;
 
   constructor(
-    private scriptsService: ScriptsService
+    private scriptsService: ScriptsService,
+    private productsService: ProductsService
   ) { }
 
   ngOnInit() {
     setTimeout(() => {
       this.scriptsService.prepareJquery();
        },1000)
+       this.productsService.getAllCategories(res => {
+        console.log(res);
+        this.categories = res;
+       })
   }
 
 }
