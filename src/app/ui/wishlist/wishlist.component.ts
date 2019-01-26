792433f47a42139d3a665225574b96687c32be53
jayson <jaysondelosreyes2022@gmail.com>
2019-01-26 13:33:53 +0800

    download pdf with empty content

--- src/app/ui/wishlist/wishlist.component.ts
+++ src/app/ui/wishlist/wishlist.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'wishlist',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WishlistComponent implements OnInit {
 
+  @ViewChild('content') content: ElementRef
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  public generatePDF() {
+    let doc = new jsPDF();
+
+    
+    let specialElementHandlers = {
+      '#editor': (element, renderer) => {
+        return true;
+      }
+    }
+
+    let content = this.content.nativeElement;
+    // let content = document.getElementById('content');
+
+    doc.fromHTML(content.innerHTML, 0, 0, {
+      'width': 10,
+      'elementHandlers': specialElementHandlers
+    }, (bla) => { doc.save('test.pdf'); });
+  } 
 
 }
