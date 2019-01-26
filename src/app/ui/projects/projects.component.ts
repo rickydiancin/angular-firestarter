2bd648c195289ee51a8eebfe76f6e8cd9e046634
jayson <jaysondelosreyes2022@gmail.com>
2019-01-26 14:20:09 +0800

    crud of project

--- src/app/ui/projects/projects.component.ts
+++ src/app/ui/projects/projects.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ProductsService } from 'src/app/core/products.service';
 
 @Component({
   selector: 'projects',
@@ -7,9 +9,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  form: any;
+  projects: any;
+  isUpdate: Boolean = false;
+
+  constructor(
+    public formBuilder: FormBuilder,
+    public productServices: ProductsService
+  ) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.form = this.formBuilder.group({
+      projectName: [''],
+      description: [''],
+      code: [''],
+      location: [''],
+      dateCreated: [Date.now()],
+      isActive: [true]
+    })
+  }
 
   ngOnInit() {
+    this.getAllProjects();
+  }
+
+  createProject() {
+    console.log(this.form.value);
+    this.productServices.newProjects(this.form.value).then(() => {
+      console.log('New Project successfully added');
+      this.form.reset();
+    })
+  }
+
+  deleteProject(id) {
+    this.productServices.deleteProject(id).then(() => {
+      console.log('Project successfully deleted');
+    })
+  }
+
+  editProject(data) {
+    this.form.patchValue(data);
+    this.isUpdate = true;
+  }
+
+  updateProject() {
+    this.productServices.newProjects(this.form.value).then(() => {
+      console.log('Project successfully updated');
+      this.form.reset();
+      this.isUpdate = false;
+    })
+  }
+
+  updateCancel() {
+    this.form.reset();
+    this.isUpdate = false;
+  }
+
+  getAllProjects() {
+    this.productServices.getAllProjects().subscribe((projects) => {
+      console.log(projects)
+      this.projects = projects;
+    })
   }
 
 }
