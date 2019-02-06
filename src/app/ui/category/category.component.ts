8976b89b2d3ecf0187cb0a6cc2c90e73b072133d
jayson <jaysondelosreyes2022@gmail.com>
2019-02-06 18:48:08 +0800

    localstorage

--- src/app/ui/category/category.component.ts
+++ src/app/ui/category/category.component.ts
@@ -81,6 +81,12 @@ export class CategoryComponent implements OnInit {
     // this.getAllCategories();
     this.getAllSolutions();
 
+    if (typeof localStorage.getItem('products') === null) {
+      this.getAllProducts();
+    } else {
+      console.log(JSON.parse(localStorage.getItem('products')), 'exist')
+    }
+
     setTimeout(() => {
       this.scriptsService.prepareJquery();
        },1000)
@@ -95,9 +101,10 @@ export class CategoryComponent implements OnInit {
 
   getAllProducts() {
     this.productsService.getAllProducts().subscribe(async (res:any) => {
-      console.log(res)
-      this.productsTemp = res;
-      this.products = res;
+      // console.log(res)
+      // this.productsTemp = res;
+      // this.products = res;
+      localStorage.setItem('products', JSON.stringify(res));
       // this.productsService.getAllCategories((resCategory:any) => {
       //   // this.categories = resCategory;
       //   this.categories = _(resCategory).filter({ 'parent': '' }).value();
