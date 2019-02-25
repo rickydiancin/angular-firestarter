e1ddfc4961810bb7d8e83116a2a1c0dd8d6b0ab2
jayson <jaysondelosreyes2022@gmail.com>
2019-02-25 12:22:51 +0800

    filter by solution design changed

--- src/app/core/scripts.service.ts
+++ src/app/core/scripts.service.ts
@@ -1104,7 +1104,7 @@ export class ScriptsService {
       
         /* ToolTip */
         $('[data-toggle="tooltip"]').tooltip();
-      
+        console.log($(".gallery").length > 0)
         if($(".gallery").length > 0){
           var $gallery = $('.gallery a').simpleLightbox();
         }
