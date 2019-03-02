b09c5f49ea8818ae6df455729a28560d17d44caf
jayson <jaysondelosreyes2022@gmail.com>
2019-03-02 13:04:45 +0800

    add mouseenter to map

--- src/app/ui/distribution/distribution.component.ts
+++ src/app/ui/distribution/distribution.component.ts
@@ -59,14 +59,18 @@ export class DistributionComponent implements OnInit {
   for (i = 0; i < locations.length; i++) {
       marker = new google.maps.Marker({
         position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-        map: map,
-        title: 'Snazzy!'
+        map: map
       });
 
-    google.maps.event.addListener(marker, 'click', (function (marker, i) {
+    google.maps.event.addListener(marker, 'mouseover', ((marker, i) => {
       return function () {
         infowindow.setContent(locations[i][0]);
         infowindow.open(map, marker);
+      }
+    })(marker, i));
+    google.maps.event.addListener(marker, 'mouseout', ((marker, i) => {
+      return function () {
+        infowindow.close();
       }
     })(marker, i));
   }
