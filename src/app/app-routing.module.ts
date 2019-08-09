37b625dbec9bccdef69be5079e8829a95ef76456
jayson <jaysondelosreyes.dev@gmail.com>
2019-08-09 15:01:07 +0800

    fixed menubar and scrollPositionRestoration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ import { UserProfileComponent } from './ui/user-profile/user-profile.component';
 import { PageContentComponent } from './ui/page-content/page-content.component';
 
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'login', component: UserLoginComponent },
   { path: 'profile', component: UserProfileComponent },
